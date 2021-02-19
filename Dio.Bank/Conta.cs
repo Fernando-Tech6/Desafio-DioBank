f197448054fa95e6cbab2fb9991bac09be59c99e
Fernando Vieira <ferdalthor@outlook.com>
2021-02-19 16:35:15 -0300

    Desafio DIO

---
+++ Dio.Bank/Conta.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dio.Bank
+{
+    public class Conta
+    {
+        
+
+        private TipoConta tipoConta { get; set; }
+        private string Nome { get; set; }
+        private double Saldo { get; set; }
+        private double Credito { get; set; }
+
+
+        public Conta(TipoConta tipoConta, string nome, double saldo, double credito)
+        {
+            this.tipoConta = tipoConta;
+            Nome = nome;
+            Saldo = saldo;
+            Credito = credito;
+        }
+
+
+    }
+
+    
+}
---
+++ Dio.Bank/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Dio.Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta contaFernando = new Conta(TipoConta.PessoaFisica,"Fernando",500,562);
+
+
+
+            Console.WriteLine(contaFernando);
+           
+        }
+    }
+}
